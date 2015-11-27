af53cfcf504f3a227160eda2c60bc1c279c1b6a7
mySmileHan <649628511@qq.com>
2015-11-27 11:37:34 +0800

    24

--- server/src/main/java/database/DriverDBManager.java
+++ server/src/main/java/database/DriverDBManager.java
@@ -1,15 +1,63 @@
 package database;
 
-import java.sql.Driver;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 import po.infopo.DriverPO;
 
 public class DriverDBManager extends DBManager{
 	
-	public void add(DriverPO po){
-		String iDString = po.getDriverID();
+	public void add(DriverPO po) throws SQLException{
+		String driverid = po.getDriverID();
 		String name = po.getName();
+		Date birthday = po.getBirthday();
+		String idnum = po.getIDNum();
+		String phonenum = po.getPhoneNum();
+		String gender = po.getGender();
+		Date licenselimit = po.getLicenseLimit();
 		
+		String driveradd = "INSERT INTO Driver "
+				+ "VALUES (?, ?, ?, ?, ?, ?, ?)";
+		Connection connection = connectToDB();
+		PreparedStatement statement = connection.prepareStatement(driveradd);
+		statement.setString(1, driverid);
+		statement.setString(2, name);
+		statement.setDate(3, birthday);
+		statement.setString(4, idnum);
+		statement.setString(5, phonenum);
+		statement.setString(6, gender);
+		statement.setDate(7, licenselimit);
+		statement.executeUpdate();		
+		stopconnection(connection);
+	}
+	
+	public void delete(String driverid) throws SQLException{
+		String driverdelete = "DELETE FROM Driver WHERE DriverID = '" + driverid + "'";
+		Connection connection = connectToDB();
+		Statement statement = connection.createStatement();
+		statement.executeUpdate(driverdelete);
+		stopconnection(connection);		
+	}
+	
+	public ArrayList<DriverPO> getAll() throws SQLException{
+		ArrayList<DriverPO> drivers = new ArrayList<DriverPO>();
+		String drivergetall = "SELECT * FROM Driver";
+		Connection connection = connectToDB();
+		Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery(drivergetall);
+		while(resultSet.next()){
+			DriverPO driver = new DriverPO(resultSet.getString(1), resultSet.getString(2),
+					resultSet.getDate(3), resultSet.getString(4), resultSet.getString(5),
+					resultSet.getString(6), resultSet.getDate(7));
+			drivers.add(driver);
+		}
+		stopconnection(connection);
+		return drivers;
 	}
 
 }
---
+++ server/src/main/java/database/StaffDBManager.java
@@ -0,0 +1,75 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import po.infopo.StaffPO;
+import typeDefinition.Job;
+
+public class StaffDBManager extends DBManager{
+
+	public void add(StaffPO po) throws SQLException{
+		String staffid = po.getStaffID();
+		String name = po.getName();
+		String gender = po.getGender();
+		Date birthday = po.getBirthday();
+		Job position = po.getPosition();
+		int job = position.ordinal();
+		int basicsalary = po.getBasicSalary();
+		int workfrequency = po.getWorkFrequency();
+		
+		String staffadd = "INSERT INTO Staff VALUES (?, ?, ?, ?, ?, ?, ?)";
+		Connection connection = connectToDB();
+		PreparedStatement statement = connection.prepareStatement(staffadd);
+		statement.setString(1, staffid);
+		statement.setString(2, name);
+		statement.setString(3, gender);
+		statement.setDate(4, birthday);
+		statement.setInt(5, job);
+		statement.setInt(6, basicsalary);
+		statement.setInt(7, workfrequency);
+		statement.executeUpdate();
+		stopconnection(connection);
+	}
+	
+	public void delete(String staffid) throws SQLException{
+		String staffdelete = "DELETE FROM Staff WHERE staffID = '" + staffid + "'";
+		Connection connection = connectToDB();
+		Statement statement = connection.createStatement();
+		statement.executeUpdate(staffdelete);
+		stopconnection(connection);
+	}
+	
+	public StaffPO get(String staffid) throws SQLException{
+		String staffget = "SELECT * FROM Staff WHERE staffid = '" + staffid + "'";
+		Connection connection = connectToDB();
+		Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery(staffget);
+		StaffPO staff = null;
+		while(resultSet.next()){
+			staff = new StaffPO(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3),
+					resultSet.getDate(4), Job.values()[resultSet.getInt(5)], resultSet.getInt(6));
+		}
+		return staff;			
+	}
+	
+	public ArrayList<StaffPO> getAll() throws SQLException{
+		ArrayList<StaffPO> staffs = new ArrayList<StaffPO>();
+		String staffgetall = "SELECT * FROM Staff";
+		Connection connection = connectToDB();
+		Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery(staffgetall);
+		while (resultSet.next()){
+			StaffPO staff = new StaffPO(resultSet.getString(1), resultSet.getString(2),
+						resultSet.getString(3), resultSet.getDate(4), Job.values()[resultSet.getInt(5)],
+						resultSet.getInt(6));
+			staffs.add(staff);
+		}
+		return staffs;
+	}
+}
