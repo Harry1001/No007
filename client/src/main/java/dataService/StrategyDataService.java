d521ec8ee8783e365d51212422b219d22b4d70c5
mySmileHan <649628511@qq.com>
2015-12-16 20:36:47 +0800

    24

--- client/src/main/java/dataService/StrategyDataService.java
+++ client/src/main/java/dataService/StrategyDataService.java
@@ -1,48 +1,51 @@
 package dataService;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.sql.SQLException;
 
 import po.strategypo.CarriageFeePO;
 import po.strategypo.DistancePO;
 import po.strategypo.ExpressFeePO;
 import po.strategypo.SalaryPO;
 
-public interface StrategyDataService {
-    /*
+public interface StrategyDataService extends Remote{
+    /**
      * 提供快递费策略
      */
-	public ExpressFeePO getExpressFee() throws RemoteException;
-	/*
-	 * 更新快递策略
-	 */
-	public void updateExpressFeeStrategy(ExpressFeePO efpo) throws RemoteException;
-	/*
+	public ExpressFeePO getExpressFee() throws  RemoteException, SQLException;
+	/**
 	 * 提供运费策略
 	 */
-	public CarriageFeePO getCarriageFee() throws RemoteException;
-	/*
+	public CarriageFeePO getCarriageFee() throws  RemoteException, SQLException;
+	/**
+	 * 更新快递策略
+	 */
+	public void updateExpressFeeStrategy(ExpressFeePO efpo) throws  RemoteException, SQLException;
+	/**
 	 * 更新运费策略
 	 */
-	public void updateCarriageFeeStrategy(CarriageFeePO cfpo) throws RemoteException;
-	/*
+	public void updateCarriageFeeStrategy(CarriageFeePO cfpo)throws  RemoteException, SQLException;
+	/**
 	 * 提供薪水策略
 	 */
-	public SalaryPO getSalary() throws RemoteException;
-	/*
+	public SalaryPO getSalary() throws  RemoteException, SQLException;
+	/**
 	 * 更新薪水策略
 	 */
-	public void updateSalaryStrategy(SalaryPO po) throws RemoteException;
-	/*
-	 * 制定城市间距离
+	public void updateSalaryStrategy(SalaryPO po) throws  RemoteException, SQLException;
+	/**
+	 * 提供城市距离常量
 	 */
-	public double getDistance(String city1,String city2)throws RemoteException;
-	/*
-	 * 提供城市间距离
+	public double getDistance(String city1,String city2) throws RemoteException, FileNotFoundException, ClassNotFoundException, IOException;
+	/**
+	 * 提供距离策略
 	 */
-	public DistancePO getDistanceStrategy()throws RemoteException;
-	/*
-	 * 更新城市间距离
+	public DistancePO getDistanceStrategy() throws RemoteException, FileNotFoundException, ClassNotFoundException, IOException;
+	/**
+	 * 更新城市距离常量
 	 */
-	public void updataDistanceStrategy(DistancePO po)throws RemoteException;
-	
+	public void updateDistanceStrategy(DistancePO po) throws RemoteException, FileNotFoundException, IOException;
 }
