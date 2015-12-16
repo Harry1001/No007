d15b9f063e4998f21b854cf46648259882f6518f
mySmileHan <649628511@qq.com>
2015-12-16 18:59:45 +0800

    24

--- client/src/main/java/dataService/_RMI.java
+++ client/src/main/java/dataService/_RMI.java
@@ -2,7 +2,7 @@ package dataService;
 
 public class _RMI {
 
-	private static String ip = "114.212.43.14:8080";
+	private static String ip = "192.168.191.3:8080";
 	
 	public static String getIP(){
 		return ip;
