42f5adcde476e734e7e8f714e988e46f4884f124
Harry1001 <benchaodong@gmail.com>
2015-12-22 20:51:05 +0800

    改了ip

--- client/src/main/java/dataService/_RMI.java
+++ client/src/main/java/dataService/_RMI.java
@@ -2,7 +2,7 @@ package dataService;
 
 public class _RMI {
 
-	private static String ip = "127.0.0.1:8081";
+	private static String ip = "172.26.208.203:8081";
 	
 	public static String getIP(){
 		return ip;
