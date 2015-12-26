70e83a16863dcbb527950fcbffc0bb2f95055f56
mySmileHan <649628511@qq.com>
2015-12-26 20:51:54 +0800

    23

--- client/src/main/java/dataService/_RMI.java
+++ client/src/main/java/dataService/_RMI.java
@@ -1,10 +1,36 @@
 package dataService;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
 public class _RMI {
 
 	private static String ip = "127.0.0.1:8081";
 	
 	public static String getIP(){
 		return ip;
+	}
+	
+	public static void setIP() {
+		File file = new File("RMI_.txt");
+		try {
+			@SuppressWarnings("resource")
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
+			ip = bufferedReader.readLine();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
